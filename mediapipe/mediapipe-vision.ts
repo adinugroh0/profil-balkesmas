1a4429381710a8b9edf0bb1584a34931c1b558d3
adinugroh0 <aditwist1@gmail.com>
2025-01-16 10:07:11 +0700

    edit

--- mediapipe/mediapipe-vision.ts
+++
@@ -1,8 +0,0 @@
-import { VISION_URL } from "@/utils/definitions";
-import { FilesetResolver } from "@mediapipe/tasks-vision";
-
-const initMediaPipVision = async () => {
-    return await FilesetResolver.forVisionTasks(VISION_URL);
-};
-
-export default initMediaPipVision;
